eba8d3a6d66afcb40464f0c48516a4c55906dc62
Nicolò Maria Mezzopera <nico@pulilab.com>
2018-12-14 17:19:58 +0100

    feat: first pie-charts and tag recognition functions

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="https://www.vuepeople.org/_nuxt/img/logo-vuepeople.26e761f.svg"
+    />
+    <section>
+      <h2>Top Ten</h2>
+      <TagChart :graph-data="tags.slice(0, 10)" />
+    </section>
+
+    <section v-for="s in sections" :key="s">
+      <h2>{{ s }}</h2>
+      <TagChart :graph-data="tags" :subsets="subsets[s]" />
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import tags from "./assets/tags.json";
+import subsets from "./assets/subsets.json";
+import TagChart from "./components/TagChart.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
-  }
+    TagChart
+  },
+  data() {
+    return {
+      tags,
+      subsets
+    };
+  },
+  computed: {
+    sections() {
+      return Object.keys(this.subsets);
+    }
+  },
+  mounted() {},
+  methods: {}
 };
 </script>
 
@@ -24,5 +51,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo {
+  width: 500px;
+}
+
+h2 {
+  text-transform: capitalize;
+}
+
+section {
+  width: 40%;
+  margin: auto;
 }
 </style>
---
+++ src/components/TagChart.vue
@@ -0,0 +1,85 @@
+<script>
+import kebabCase from "lodash/kebabCase";
+import { Pie } from "vue-chartjs";
+
+export default {
+  extends: Pie,
+  props: {
+    graphData: {
+      type: Array,
+      default: null
+    },
+    subsets: {
+      type: Array,
+      default: null
+    }
+  },
+  computed: {
+    filteredData() {
+      return this.subsets ? this.filterBySubset() : this.graphData;
+    },
+    chartData() {
+      const data = [];
+      const labels = [];
+      const backgroundColor = [];
+      this.filteredData.forEach(gd => {
+        data.push(gd.people);
+        labels.push(gd.name);
+        backgroundColor.push(this.officiallyRandomColors(gd.name));
+      });
+      return {
+        datasets: [
+          {
+            data,
+            backgroundColor
+          }
+        ],
+        labels
+      };
+    }
+  },
+  mounted() {
+    this._options = {
+      responsive: true
+    };
+    this.renderChart(this.chartData, this._options);
+  },
+  methods: {
+    officiallyRandomColors(name) {
+      switch (name) {
+        case "vue": {
+          return "#42b883";
+        }
+        default: {
+          return "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+        }
+      }
+    },
+    filterBySubset() {
+      const finalTags = [];
+      const parsed = this.subsets.reduce((a, c) => {
+        if (c instanceof Array) {
+          const inner = { people: 0 };
+          c.forEach(innerTag => {
+            a[innerTag.toLowerCase()] = inner;
+          });
+          finalTags.push(c[0].toLowerCase());
+        } else {
+          a[c.toLowerCase()] = { people: 0 };
+          finalTags.push(c.toLowerCase());
+        }
+        return a;
+      }, {});
+      this.graphData.forEach(d => {
+        const parsed_name = kebabCase(d.name);
+        if (parsed[parsed_name] !== undefined) {
+          parsed[parsed_name].people += d.people;
+        }
+      });
+      return finalTags.map(f => ({ name: f, people: parsed[f].people }));
+    }
+  }
+};
+</script>
+
+<style></style>
